82682d56141b0f6ac69ad42b1243ef930445f842
Fernanda Marroquin <fernanda.marroquin07@gmail.com>
2019-03-02 13:40:08 -0500

    add schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,4 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
-
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+-- Drops the gamelist if it exists currently --
+DROP DATABASE IF EXISTS gamelist;
+-- Creates the "gamelist" database --
+CREATE DATABASE gamelist;
