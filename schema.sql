7c38af3104310c0e2d68135c0611a49b174f20c6
Fernanda Marroquin <34196754+fernandamarr@users.noreply.github.com>
2019-03-09 12:31:59 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,11 +2,3 @@
 DROP DATABASE IF EXISTS gamelist;
 -- Creates the "gamelist" database --
 CREATE DATABASE gamelist;
-
-CREATE TABLE Users
-(
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
-);
